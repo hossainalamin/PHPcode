ad65d834fed6b09040791152a5c3e3341839afb0
Md Alamin Hossain <hossainalamin980@gmail.com>
2020-10-13 14:22:43 +0600

    Create cookie.php

---
+++ OOP PHP1/cookie.php
@@ -0,0 +1,22 @@
+<form action="" method="post">
+  <input type="text" name="shirt" placeholder="Enter name">
+  <input type="submit" name="submit" value="Submit">
+</form>
+<?php
+//create
+//check
+if(isset(_POST['submit'])){
+  if(count($_COOKIE) >0)
+    echo "you already have created ".count($_COOKIE)." cookies";
+  else
+      setcookie('shirt',$_POST['shirt'],time()+60*60*60*24);
+}
+//delete
+setcookie('shirt',$_POST['shirt'],time()-60*60*60);
+if($_COOKIE['shirt']){
+  echo "please buy a shirt";
+else{
+  echo "please buy a shirt";
+}
+}
+?>
